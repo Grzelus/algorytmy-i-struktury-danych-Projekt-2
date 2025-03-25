a07fef45a0fa4e98537c3565086fdd9222e91744
Antoni Małecki <antmalecki>
2025-03-25 20:38:29 +0100

    generator added

---
+++ tree.py
@@ -0,0 +1,23 @@
+import time
+from datetime import timedelta
+import random
+import sys
+
+sys.setrecursionlimit(100000)
+
+def generate(n,min_value, max_value):
+    if(min_value > max_value):
+        print("minimalna wartość nie powinna być większa od maksymalnej.")
+        return []
+    output = [random.randint(min_value,max_value) for _ in range(n)]
+    output = [random.randint(min_value,max_value) for _ in range(n)]
+
+    choice = int(input("Wybierz sposób sortowania wygenerowanych liczb \n 1) losowo\n 2) rosnąco "))
+
+    if choice == 1:
+        return output
+    if choice == 2:
+        output.sort()
+        return output
+    print("wybierz właściwą opcje")
+    return []
