b2790d99e15510a6815d993a861f51faf4840cb6
Kacper Grzelak <upsetboii@gmail.com>
2025-03-31 19:47:40 +0200

    basic menu

---
+++ app.py
@@ -0,0 +1,75 @@
+from node import Node
+from avl import AVL
+from bst import BST
+from heapmin import HeapMinTree
+
+import time
+from datetime import timedelta
+import random
+
+def read_from_file(filename):
+    with open(filename, 'r') as f:
+        content = f.read()
+    numbers = list(map(int, content.strip().split()))
+    return numbers
+
+def generate(n,min_value, max_value):
+    if(min_value > max_value):
+        print("minimalna wartość nie powinna być większa od maksymalnej.")
+        return []
+    
+    output = [random.randint(min_value,max_value) for _ in range(n)]
+
+    choice = int(input("Wybierz sposób sortowania wygenerowanych liczb \n 1) losowo\n 2) rosnąco "))
+
+    if choice == 1:
+        return output
+    if choice == 2:
+        output.sort()
+        return output
+    print("wybierz właściwą opcje")
+    return []
+
+def create_input_data(choice):
+    if input_type == 1:
+        file_name = input("podaj nazwę pliku")
+        data = read_from_file("filename")
+    elif input_type == 2:
+        n = int(input("Ile liczb chcesz wygenerować?"))
+        min_value = int(input("Podaj wartość minimalną losowanych liczb:"))
+        max_value = int(input("Podaj wartość maksymalną losowanych liczb:"))
+        data = generate(n, min_value, max_value)
+    else:
+        print("Wybierz właściwą opcję.")
+        data = []
+
+    return data
+    
+
+
+tree_type = int(input("Jaki rodzaj drzewa chcesz zbudować:\n1) BST\n2) AVL\n 3) Heap Min"))
+input_type = int(input("Skąd chcesz pobrać dane:\n1) Z pliku tekstowego\n2) generowane losowo"))
+
+data = create_input_data(input_type)
+
+tree = None
+
+if tree_type == 1:
+    tree = BST()
+    for num in data:
+        tree.insert(num)
+
+if tree_type == 2:
+    tree = AVL()
+    data = data.sort()
+    tree.build_from_sorted_list(data)
+
+if tree_type == 3:
+    tree = HeapMinTree()
+    for num in data:
+        tree.insert(num)
+
+else:
+    print("niepoprawny wybór drzewa")
+
+
--- tree.py
+++ tree.py
@@ -10,7 +10,6 @@ def generate(n,min_value, max_value):
         print("minimalna wartość nie powinna być większa od maksymalnej.")
         return []
     output = [random.randint(min_value,max_value) for _ in range(n)]
-    output = [random.randint(min_value,max_value) for _ in range(n)]
 
     choice = int(input("Wybierz sposób sortowania wygenerowanych liczb \n 1) losowo\n 2) rosnąco "))
 
@@ -22,23 +21,5 @@ def generate(n,min_value, max_value):
     print("wybierz właściwą opcje")
     return []
 
-class Node:
-    def __init__(self,key):
-        self.key=key
-        self.right=None
-        self.left=None
-        self.level=None
-    def __show__(self):
-        print(f"{self.key} - {self.data} - {self.level}") 
 
-def insert(parent, child,depth=0):
-    if(parent == None):
-        child.depth=depth
-        return child
-    elif(child.id==parent.id):
-        return parent
-    elif(child.id>parent.id):
-        parent.left=insert(parent.left,child,depth+=1)
-    else:
-        parent.right=insert(parent.right,child,depth+=1)
-    return parent
+
